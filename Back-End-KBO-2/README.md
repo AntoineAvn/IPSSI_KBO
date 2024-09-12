63638b54632f9bb7b48c89e28150d02fde101e18
AxelSELEBRAN <a.selebran@tsf.fr>
2024-09-12 10:28:29 +0200

    test

---
+++ Back-End-KBO-2/README.md
@@ -0,0 +1,7 @@
+### Initiation Docker MongoDB
+
+   1. **Exécuter cette commande pour build l'image docker**
+
+    ``bash
+        docker compose --env-file .env up --build
+    ``
---
+++ Back-End-KBO-2/src/utils/conn.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+
+dotenv.config()
+
+const uri = process.env.URL_MONGO || ''
+
+console.log("lien bdd: ",uri)
+
+mongoose.connect(uri);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+export default db;
